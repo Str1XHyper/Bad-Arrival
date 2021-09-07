ce94f1196edb5a91bc29b2ecf13d123fbb00fb36
Tijn van Veghel <tijnvanveghel@gmail.com>
2021-09-07 17:08:45 +0200

    Added colors to inventory

--- Bad Arrival/Assets/Scripts/Inventory System/UserInterface.cs
+++ Bad Arrival/Assets/Scripts/Inventory System/UserInterface.cs
@@ -12,6 +12,12 @@ public abstract class UserInterface : MonoBehaviour
 
     public InventoryObject inventory;
     public Dictionary<GameObject, InventorySlot> slotsOnInterface = new Dictionary<GameObject, InventorySlot>();
+    [SerializeField] private Color CommonColor;
+    [SerializeField] private Color UncommonColor;
+    [SerializeField] private Color RareColor;
+    [SerializeField] private Color EpicColor;
+    [SerializeField] private Color LegendaryColor;
+
     void Start()
     {
         for (int i = 0; i < inventory.GetSlots.Length; i++)
@@ -125,18 +131,46 @@ public static class MouseData
 
 public static class ExtensionMethods
 {
+
     public static void UpdateSlotDisplay(this Dictionary<GameObject, InventorySlot> _slotsOnInterface)
     {
+        Color CommonColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        Color UncommonColor = new Color(0.5f, 0.7f, 0.5f, 0.5f);
+        Color RareColor = new Color(0.5f, 0.6f, 0.7f, 0.5f);
+        Color EpicColor = new Color(0.6f, 0.5f, 0.7f, 0.5f);
+        Color LegendaryColor = new Color(0.7f, 0.6f, 0.5f, 0.5f);
         foreach (KeyValuePair<GameObject, InventorySlot> _slot in _slotsOnInterface)
         {
             if (_slot.Value.item.Id >= 0)
             {
+                Color bgColor;
+                switch (_slot.Value.ItemObject.Rarity)
+                {
+                    case Rarities.Common:
+                    default:
+                        bgColor = CommonColor;
+                        break;
+                    case Rarities.Uncommon:
+                        bgColor = UncommonColor;
+                        break;
+                    case Rarities.Rare:
+                        bgColor = RareColor;
+                        break;
+                    case Rarities.Epic:
+                        bgColor = EpicColor;
+                        break;
+                    case Rarities.Legendary:
+                        bgColor = LegendaryColor;
+                        break;
+                }
+                _slot.Key.GetComponent<Image>().color = bgColor;
                 _slot.Key.transform.GetChild(0).GetComponentInChildren<Image>().sprite = _slot.Value.ItemObject.uiDisplay;
                 _slot.Key.transform.GetChild(0).GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
                 _slot.Key.GetComponentInChildren<TextMeshProUGUI>().text = _slot.Value.amount == 1 ? "" : _slot.Value.amount.ToString("n0");
             }
             else
             {
+                _slot.Key.GetComponent<Image>().color = CommonColor;
                 _slot.Key.transform.GetChild(0).GetComponentInChildren<Image>().sprite = null;
                 _slot.Key.transform.GetChild(0).GetComponentInChildren<Image>().color = new Color(1, 1, 1, 0);
                 _slot.Key.GetComponentInChildren<TextMeshProUGUI>().text = "";
